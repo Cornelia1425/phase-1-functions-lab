d5caa1e49ee42ddd23232ce2f11642ed6660a893
yiranshu <shuyiran1425@hotmail.com>
2024-03-18 21:04:57 -0400

    function

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street-42)
+}
+
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street)*264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start-destination)*264)
+
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = Math.abs((start-destination)*264)
+    let fare 
+    switch (true){ //why it's true? 
+        //using switch (true) allows you to evaluate conditions within the switch statement and return the desired value based on whether the condition is true or false.
+        case distance <= 400:
+            return 0
+     
+        case distance>400 && distance<=2000:
+            fare = (distance -400)*2*0.01;
+            return fare;
+
+        case distance>2000 && distance <=2500:
+            fare = 25;
+            return fare
+
+        default:
+            return 'cannot travel that far'
+    }
+
+}
